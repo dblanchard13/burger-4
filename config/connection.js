aeb9f365b9991b4b1450a6a380fea036835337c3
Robin Mi <si.mi5689@gmail.com>
2017-05-06 00:44:49 -0700

    changed connection.js to allow jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var configs = require("./configs.js");
+var connection
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) 
+{
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
